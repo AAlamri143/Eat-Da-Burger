a19e981d3c93101b94cb9aba7e80fb64c7c4641c
Abdullah Alamri <cast@Abdullahs-MacBook.local>
2019-02-28 23:38:06 -0500

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Classic Cheeseburger',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bleu Cheese Burger',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Chicken Burger',0);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Beef Burger',0);
+
